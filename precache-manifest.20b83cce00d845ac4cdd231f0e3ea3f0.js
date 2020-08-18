245073d24e1a8a17ac241f65c2b11f13cbd8c25f
demian <demianz999@gmail.com>
2020-08-17 21:41:11 -0500

    Updates

---
+++ precache-manifest.20b83cce00d845ac4cdd231f0e3ea3f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d50a6b2ca02bfc723f6de20fa8de95b",
+    "url": "/reactPasteleriaFrontend/index.html"
+  },
+  {
+    "revision": "d84ac7abdca0651be55b",
+    "url": "/reactPasteleriaFrontend/static/css/2.63ba52ef.chunk.css"
+  },
+  {
+    "revision": "b99125703c45da977b15",
+    "url": "/reactPasteleriaFrontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d84ac7abdca0651be55b",
+    "url": "/reactPasteleriaFrontend/static/js/2.059d210b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPasteleriaFrontend/static/js/2.059d210b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99125703c45da977b15",
+    "url": "/reactPasteleriaFrontend/static/js/main.47b4c879.chunk.js"
+  },
+  {
+    "revision": "ef1ba85451240e5cb2d5",
+    "url": "/reactPasteleriaFrontend/static/js/runtime-main.ad766385.js"
+  }
+]);
